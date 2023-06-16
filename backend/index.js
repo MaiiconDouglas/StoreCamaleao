3b57faa20c92d5246893aa44c1ee9e5e26a0424a
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-06-16 07:41:08 -0300

    update #05

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,8 @@
 const bodyParser = require("body-parser");
-
 const express = require("express");
-
 const dbConnect = require("./config/dbConnect");
-
 const app = express();
-
 const dotenv = require("dotenv").config();
-
 const PORT = process.env.PORT || 4000;  // PORTA DO SERVIDOR 4000
 
 const authRouter = require("./routes/authRoute");
@@ -15,9 +10,7 @@ const authRouter = require("./routes/authRoute");
 dbConnect();  // Conecta com o banco de dados
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
