359c90523dd5ffe27489dc0239a92de9fa2a728b
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-06-22 14:44:03 -0300

    APP OK!

--- mobileAPP/App.js
+++ mobileAPP/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-// 1. import `NativeBaseProvider` component
 import { NativeBaseProvider, Text, Box } from "native-base";
+import HomeScreen from "./src/Screens/HomeScreen";
 
 export default function App() {
-  // 2. Use at the root of your app
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Aplicativo Funcionando!</Text>
-      </Box>
+      <HomeScreen />
     </NativeBaseProvider>
   );
 }
